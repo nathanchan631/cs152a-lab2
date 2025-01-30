46d55f0d30198812e46e3057aacf5170dcea8c30
Nathan Chan <nathanchan631@gmail.com>
2025-01-30 14:32:07 -0800

    signed magnitude conversion working

--- fpcvt.v
+++ fpcvt.v
@@ -1,37 +1,19 @@
 module fpcvt(
     // Outputs
-    S,
-    E,
-    F,
+    output wire S,
+    output wire [2:0] E,
+    output wire [3:0] F,
+    output wire [11:0] debug,
     // Inputs
-    D
+    input [11:0] D
     );
 
-    output wire S;
-    output wire[2:0] E;
-    output wire[3:0] F;
-    input [11:0] D;
+    wire [11:0] mag;
 
-    task twosCompToSignMag;
-        input [11:0] twosComp;
-        output [11:0] mag;
-        output S;
-        begin
-            // negative numbers
-            if (twosComp[11] == 1'b0) begin
-                mag = twosComp;
-                sign = 1'b0;
-            end
-            // -2048
-            else if (twos_complement == 12'b100000000000) begin
-                mag = 12'b011111111111;
-                S = 1'b1;
-            end
-            // general case
-            else begin
-                mag = ~twos_complement + 1'b1;
-                S = 1'b1;
-            end
-        end
-    endtask
+    assign S = D[11];
+    assign mag = (S == 1'b0) ? D : 
+                    ((D == 12'b100000000000) ? 12'b011111111111 : (~D + 1'b1));
+
+    assign debug=mag;
+
 endmodule
--- tb/tb.v
+++ tb/tb.v
@@ -1,29 +1,39 @@
+`timescale 1ns/1ps
+
 module tb;
 
-    wire [11:0] D;
+    reg [11:0] D;
     wire S;
     wire [2:0] E;
     wire [3:0] F;
 
-    wire[11:0] mag;
+    wire[11:0] debug;
 
     initial begin
         $dumpfile("wave.vcd");
         $dumpvars(0, tb);
     end 
 
-    initial begin
-        D = 12'b000110100110
-
-        twosCompToSignMag(D, mag, S);
-        $finish
-    end
-
     fpcvt fpcvt_(
         .D (D),
         .S (S),
         .E (E),
-        .F (F)
+        .F (F),
+        .debug (debug)
     );
+
+    initial begin
+        $monitor("Time = %0t | D = %b | S = %b | E = %b | F = %b | Debug= %b", 
+                 $time, D, S, E, F, debug);
+
+        D = 12'b000000000000; #10;  // 0
+        D = 12'b000000000001; #10;  // 1
+        D = 12'b000000101010; #10;  // 42
+        D = 12'b100000000000; #10;  // -2048
+        D = 12'b111111111111; #10;  // -1
+        D = 12'b111111101110; #10;  // -34
+
+        $finish;
+    end
 
 endmodule
